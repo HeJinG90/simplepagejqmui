2ee520a7ddc05eefb93aaa550970a82e8bbe4a4a
hejingang <623350934@qq.com>
2018-06-04 16:53:19 +0800

    no message

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@
 			if(url==null){
 				common.router(window.href.sub1); //如果sessionStorage没有值  跳转到默认子页
 			}else{
-				common.router(url);
+				common.router(url);//如果有值  刷新页面依然停留在本页
 			}
 			
 		},
